0a78369d3bda2be58c6964635601fb5dad4b9ff1
kiplangatbryan <kiplangatsgt@gmail.com>
2022-06-27 17:26:22 +0300

    created map selection page

--- lib/pages/launderMarts/onboarding/pages/location.dart
+++ lib/pages/launderMarts/onboarding/pages/location.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PickUpspotsSelection extends StatefulWidget {
   const PickUpspotsSelection({Key? key}) : super(key: key);
@@ -10,13 +11,49 @@ class PickUpspotsSelection extends StatefulWidget {
 }
 
 class _PickUpspotsSelectiontate extends State<PickUpspotsSelection> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(0.28927317965072585, 35.29253462041064),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(
-        child: Text('Hey'),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        
+        label: Text("Lake Side"),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
