d6bedfa0cf872e0dc317ef90a71697a6150879bf
kiplangatbryan <kiplangatsgt@gmail.com>
2022-06-27 17:25:52 +0300

    created pick up selection page

---
+++ lib/pages/launderMarts/onboarding/pages/pickup_select.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:msafi_mobi/components/form_components.dart';
+
+class LocationFeedScreen extends StatefulWidget {
+  const LocationFeedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LocationFeedScreen> createState() => _LocationFeedScreenState();
+}
+
+class _LocationFeedScreenState extends State<LocationFeedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 8,
+            ),
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "How many pick up locations do you have",
+                        style: GoogleFonts.poppins(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                customButton(
+                  title: "Next",
+                  role: "next",
+                  callback: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
