461e564ccc355b7e10d38833fa60a63f948ca2fd
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-01 12:54:53 +0300

    api keys swap

--- lib/services/map.services.dart
+++ lib/services/map.services.dart
@@ -4,17 +4,22 @@ import 'dart:convert';
 import 'dart:collection';
 
 class MapServices {
-  final String key = "AIzaSyBHZxLxp45JwsG-8T2jCTcBlbrVv5Vjxs0";
+  final String key = "AIzaSyASBc-Ac9yrei1uG8iPF4DJvkQf8slxjlw";
   final String types = "geocode";
 
   Future<List> searchPlaces(String searchInput) async {
-    final String url =
-        "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchInput&types=$types&key=$key";
-    var response = await http.get(Uri.parse(url));
-    var json = jsonDecode(response.body);
-    // type casting
-    var results = json['predictions'] as List;
+    try {
+      final String url =
+          "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchInput&types=$types&key=$key";
+      var response = await http.get(Uri.parse(url));
+      print(response.body);
+      var json = jsonDecode(response.body);
+      // type casting
+      var results = json['predictions'] as List;
 
-    return results;
+      return results;
+    } catch (err) {
+      rethrow;
+    }
   }
 }
