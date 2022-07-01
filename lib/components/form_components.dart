30c702332ce622fcfc51d3a03c915484c99e2f3f
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-01 12:56:15 +0300

    added custom password widget

--- lib/components/form_components.dart
+++ lib/components/form_components.dart
@@ -30,7 +30,7 @@ Container MergeButton({required BuildContext context, required onPressed}) {
   );
 }
 
-Widget customTextField({
+Widget customPasswordField({
   required String hint,
   required String label,
   required TextInputType inputType,
@@ -47,6 +47,69 @@ Widget customTextField({
     onFieldSubmitted: (val) => onSubmit(val),
     cursorColor: kTextColor,
     cursorHeight: 20,
+    keyboardType: inputType,
+    obscureText: true,
+    // ? TextInputType.visiblePassword
+    // : TextInputType.emailAddress,
+    style: GoogleFonts.poppins(
+      color: kTextColor,
+      fontWeight: FontWeight.w600,
+      fontSize: 17,
+    ),
+    decoration: InputDecoration(
+      hintText: hint,
+      labelText: label,
+      labelStyle: GoogleFonts.poppins(
+        color: kTextMediumColor.withOpacity(.4),
+        fontSize: 17,
+      ),
+      floatingLabelStyle: GoogleFonts.poppins(
+        color: kTextMediumColor.withOpacity(.4),
+        fontSize: 17,
+      ),
+      filled: true,
+      fillColor: kTextMediumColor.withOpacity(.06),
+      prefixIcon: icon,
+
+      // ? const Icon(Icons.mail_outline, size: 18)
+      // : const Icon(Icons.lock_open_outlined, size: 18),
+      floatingLabelBehavior: FloatingLabelBehavior.auto,
+      contentPadding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: const BorderSide(
+          color: Colors.transparent,
+        ),
+        gapPadding: 10,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(
+          color: kTextMediumColor.withOpacity(.4),
+        ),
+        gapPadding: 10,
+      ),
+    ),
+  );
+}
+
+Widget customTextField({
+  required String hint,
+  required String label,
+  required TextInputType inputType,
+  required Icon icon,
+  required Function validator,
+  required Function onChanged,
+  required Function onSubmit,
+  int? minLines,
+  int? maxLines,
+}) {
+  return TextFormField(
+    validator: (val) => validator(val),
+    onChanged: (val) => onChanged(val),
+    onFieldSubmitted: (val) => onSubmit(val),
+    cursorColor: kTextColor,
+    cursorHeight: 17,
     keyboardType: inputType,
     minLines: minLines,
     maxLines: maxLines,
@@ -78,7 +141,7 @@ Widget customTextField({
       contentPadding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(10),
-        borderSide: BorderSide(
+        borderSide: const BorderSide(
           color: Colors.transparent,
         ),
         gapPadding: 10,
@@ -99,8 +162,8 @@ GestureDetector customButton(
   return GestureDetector(
     onTap: callback,
     child: Container(
-      padding: EdgeInsets.symmetric(
-        vertical: 13,
+      padding: const EdgeInsets.symmetric(
+        vertical: 12,
       ),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
@@ -115,20 +178,23 @@ GestureDetector customButton(
         children: [
           Text(
             title,
-            style: GoogleFonts.poppins(
-              fontSize: 21.0,
-              letterSpacing: 2.0,
+            style: GoogleFonts.notoSans(
+              fontSize: 18.0,
+              letterSpacing: 1.3,
               fontWeight: FontWeight.bold,
               color: role == "login" ? kTextColor : Colors.white,
               decorationStyle: TextDecorationStyle.wavy,
             ),
           ),
-          SizedBox(
-            width: 18,
+          const SizedBox(
+            width: 13,
           ),
           role != "login"
-              ? Icon(Icons.login)
-              : Image(
+              ? const Icon(
+                  Icons.login,
+                  color: kTextLight,
+                )
+              : const Image(
                   image: AssetImage('assets/logo/google.png'),
                   height: 26,
                 ),
