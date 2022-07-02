49d446483f2bbeb346cd060c0734efc42f9289bc
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-02 19:56:29 +0300

    created mock data fpr a store

--- lib/configs/data.dart
+++ lib/configs/data.dart
@@ -13,7 +13,7 @@ List<Map> fetchClothes() {
       "imagePath": "assets/clothes/jeans.png",
     },
     {
-      "title": 'man',
+      "title": 'Coat',
       "imagePath": "assets/clothes/man.png",
     },
     {
@@ -33,8 +33,283 @@ List<Map> fetchClothes() {
       "imagePath": "assets/clothes/sweatshirt.png",
     },
     {
-      "title": 'woman-clothes',
+      "title": 'dress',
       "imagePath": "assets/clothes/woman-clothes.png",
     },
+  ];
+}
+
+List<Map> fetchOrders() {
+  return [
+    {
+      "id": "140-400",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-401",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-402",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-403",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-404",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-405",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-406",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-407",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    },
+    {
+      "id": "140-408",
+      "drop_off_time": "2022-06-28T09:22:00.595Z",
+      "expected_pick_up": "2022-06-28T09:22:00.595Z",
+      "pick_up_station": "New Times Square",
+      "clothes": [
+        {
+          "id": "dress",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "trouser",
+          "count": "1",
+          "price": 200.0,
+        },
+        {
+          "id": "hat",
+          "count": "2",
+          "price": 200.0,
+        },
+        {
+          "id": "sweat-shirt",
+          "count": "3",
+          "price": 200.0,
+        }
+      ],
+      "total": 800.0,
+      "vat": 25.0,
+    }
   ];
 }
