f5b149d686f04def6ea6a6af12c4f69a0c1c5ef6
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-05 06:25:33 +0300

    refactored custom button

--- lib/components/form_components.dart
+++ lib/components/form_components.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:msafi_mobi/themes/main.dart';
 
-SizedBox MergeButton({required BuildContext context, required onPressed}) {
+SizedBox customExtendButton(
+    {required Widget child, required Function onPressed}) {
   return SizedBox(
     width: double.infinity,
     child: ElevatedButton(
@@ -15,17 +16,12 @@ SizedBox MergeButton({required BuildContext context, required onPressed}) {
         ),
         elevation: 1,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
-      onPressed: onPressed,
+      onPressed: () => onPressed,
       autofocus: true,
-      child: Text("Login",
-          style: GoogleFonts.notoSans(
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          )),
+      child: child,
     ),
   );
 }
@@ -44,7 +40,7 @@ Widget customPasswordField({
   return TextFormField(
     validator: (val) => validator(val),
     onChanged: (val) => onChanged(val),
-    onFieldSubmitted: (val) => onSubmit(val),
+    onSaved: (val) => onSubmit(val),
     cursorColor: kTextColor,
     cursorHeight: 20,
     keyboardType: inputType,
