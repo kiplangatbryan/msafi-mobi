ab6b9ea26056c7882d5390ee6906794703772390
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-07 20:46:45 +0300

    customizing routes for different users and situations

--- src/routes/v1/launder.route.js
+++ src/routes/v1/launder.route.js
@@ -2,13 +2,21 @@ const express = require('express');
 const validate = require('../../middlewares/validate');
 const launderValidation = require('../../validations/store.validation');
 const launderController = require('../../controllers/launder.controller');
+const stationController = require('../../controllers/stations.controller');
+
 const auth = require('../../middlewares/auth');
+const { fileHandler } = require('../../middlewares/files');
 
 const router = express.Router();
 
-router.route('/createStore').post(auth(), validate(launderValidation.createStore), launderController.createStore);
-router.route('/fetchStore').get(auth(), validate(launderValidation.fetchStores), launderController.fetchStore);
+router
+  .route('/createStore')
+  .post(auth(), fileHandler('store'), validate(launderValidation.createStore), launderController.createStore);
+router.route('/fetchStore').get(auth(), launderController.fetchStore);
 router.route('/fetchStores').get(launderController.fetchAllStores);
+router.route('/fetchStations/:storeId').get(auth(), validate(launderValidation.fetchStore), stationController.fetchStations);
+// router.route('/test').post(validate(launderValidation.createStore), fileHandler('store'), launderController.createStore);
+
 router.route('/stk-push/callback').post(launderController.fetchAllStores);
 
 module.exports = router;
