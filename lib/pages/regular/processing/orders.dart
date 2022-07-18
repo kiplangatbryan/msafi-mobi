8857e3c20c25b02f0100e2cfb1592e5d58801361
kiplangatbryan <kiplangatsgt@gmail.com>
2022-07-18 20:55:04 +0300

    order caching

--- lib/pages/regular/processing/orders.dart
+++ lib/pages/regular/processing/orders.dart
@@ -8,9 +8,11 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:moment_dart/moment_dart.dart';
 import 'package:msafi_mobi/components/snackback_component.dart';
 import 'package:msafi_mobi/pages/regular/components/app_bar.dart';
 import 'package:msafi_mobi/providers/placed.providers.dart';
+import 'package:msafi_mobi/themes/main.dart';
 import 'package:provider/provider.dart';
 
 import '../../../helpers/http_services.dart';
@@ -30,11 +32,21 @@ class _OrdersViewState extends State<OrdersView> {
   @override
   void initState() {
     super.initState();
-    fetchOrders();
+    var storeOrders = context.read<ExistingOrders>().orders;
+    if (storeOrders.isEmpty) {
+      fetchOrders();
+    } else {
+      setState(() {
+        orders = storeOrders;
+      });
+    }
   }
 
   fetchOrders() async {
-    final url = Uri.parse('${baseUrl()}/store/fetchUserOrders');
+    setState(() {
+      loading = true;
+    });
+    final url = Uri.parse('${baseUrl()}/store/fetch-user-orders');
     final token = await checkAndValidateAuthToken();
     final headers = {"Authorization": "Bearer $token"};
     try {
@@ -44,12 +56,14 @@ class _OrdersViewState extends State<OrdersView> {
           );
 
       try {
-        final data = json.decode(response.body).catchError();
+        final data = json.decode(response.body);
 
         if (response.statusCode == 200) {
-          print(orders);
           if (data.isNotEmpty) {
             context.read<ExistingOrders>().populateOrders(data);
+            setState(() {
+              orders = data;
+            });
           }
         } else {
           setState(() {
@@ -81,35 +95,59 @@ class _OrdersViewState extends State<OrdersView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: mainAppBar(context: context, title: "Orders"),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).backgroundColor,
+        title: Text(
+          "My Orders",
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 20,
-          vertical: 30,
+          vertical: 10,
         ),
         child: Column(
           children: [
             SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
                   Chip(
-                    label: Row(
-                      children: [
-                        const Icon(Icons.bookmark),
-                        Text(
-                          "Completed",
-                          style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ],
+                    elevation: 2,
+                    labelPadding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 4,
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    label: Text(
+                      "Pending",
+                      style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                            color: kTextLight,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Chip(
+                    elevation: 2,
+                    labelPadding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 4,
+                    ),
+                    label: Text(
+                      "completed",
+                      style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                            color: kTextLight,
+                            fontWeight: FontWeight.bold,
+                          ),
                     ),
                   ),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 15,
-            ),
+            const SizedBox(height: 20),
             orders.isEmpty
                 ? Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -133,7 +171,88 @@ class _OrdersViewState extends State<OrdersView> {
                     shrinkWrap: true,
                     itemCount: orders.length,
                     itemBuilder: (BuildContext context, index) {
-                      return Container();
+                      final orderItem = orders[index];
+                      return Card(
+                        elevation: 5,
+                        child: InkWell(
+                          onTap: () {},
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 18,
+                            ),
+                            child: Column(
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      orderItem['alias'],
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                    ),
+                                    Chip(
+                                      label: Text(
+                                        orderItem['status'],
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .subtitle1,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "LaunderMart",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                        ),
+                                        Text(
+                                          orderItem['storeId']['name'],
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "Expected Date",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodySmall,
+                                        ),
+                                        Text(
+                                          DateTime.parse(
+                                                  orderItem['expectedPickUp'])
+                                              .toMoment()
+                                              .toString(),
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
                     },
                   ),
           ],
